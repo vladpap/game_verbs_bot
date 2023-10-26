001e9f5a808795cbf8309edf0d25ba3c31ef4b19
Vladimir Papin <vladimir.papin@gmail.com>
2023-10-26 16:22:21 +0300

    add DialogFlow learning script

--- game_verbs_bot.py
+++ game_verbs_bot.py
@@ -1,10 +1,10 @@
 import logging
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, \
-    MessageHandler, Filters, CallbackContext
-
 from environs import Env
+from google.cloud import api_keys_v2, dialogflow
+from telegram import Update
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, Updater)
 
 # Enable logging
 logging.basicConfig(
@@ -13,6 +13,20 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+def create_api_key(project_id):
+    client = api_keys_v2.ApiKeysClient()
+
+    key = api_keys_v2.Key()
+    key.display_name = "My first API key"
+
+    request = api_keys_v2.CreateKeyRequest()
+    request.parent = f"projects/{project_id}/locations/global"
+    request.key = key
+
+    response = client.create_key(request=request).result()
+    return response
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -25,12 +39,48 @@ def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
 
-def main() -> None:
+def detect_intent_text(project_id,
+                       session_id,
+                       message_to_dialogflow,
+                       language_code='ru'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(
+        text=message_to_dialogflow,
+        language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    serialized_answer = {
+        'intention': response.query_result.intent.display_name,
+        'confidence': response.query_result.intent_detection_confidence,
+        'answer': response.query_result.fulfillment_text
+    }
+    return serialized_answer
+
+
+def echo_dialogflow(update: Update, context: CallbackContext) -> None:
+    message_to_dialogflow = update.message.text
+    session_id = update.effective_chat.id
+    serialized_answer = detect_intent_text(
+        project_id,
+        session_id,
+        message_to_dialogflow)
+    update.message.reply_text(serialized_answer['answer'])
+
+
+if __name__ == '__main__':
     env = Env()
     env.read_env()
 
-    TG_TOKEN = env.str('TG_BOT_TOKEN')
+    project_id = env.str('DIALOGFLOW_PROJECT_ID')
 
+    DF_TOKEN = create_api_key(project_id)
+    print("Successfully created an API key")
+
+    TG_TOKEN = env.str('TG_BOT_TOKEN')
     updater = Updater(TG_TOKEN)
 
     dispatcher = updater.dispatcher
@@ -38,12 +88,8 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('start', start))
 
     dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, echo))
+        MessageHandler(Filters.text & ~Filters.command, echo_dialogflow))
 
     updater.start_polling()
 
     updater.idle()
-
-
-if __name__ == '__main__':
-    main()
---
+++ intent_management.py
@@ -0,0 +1,73 @@
+import argparse
+import json
+
+from environs import Env
+from google.cloud import dialogflow
+
+
+def create_intent(project_id,
+                  display_name,
+                  training_phrases_parts,
+                  message_texts):
+    """Create an intent of the given intent type."""
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def open_json_file(file_name):
+    with open(file_name, 'r') as my_file:
+        file_json = my_file.read()
+
+    return json.loads(file_json)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Script create intents on DialogFlow.')
+    parser.add_argument(
+        'file_name',
+        action='store',
+        help='Json file name (traning questions and answer.')
+
+    args = parser.parse_args()
+
+    questions = open_json_file(args.file_name)
+    indents_title = list(questions.keys())
+
+    env = Env()
+    env.read_env()
+
+    project_id = env.str('DIALOGFLOW_PROJECT_ID')
+
+    for title in indents_title:
+        indent = questions[title]
+        create_intent(
+            project_id=project_id,
+            display_name=title,
+            training_phrases_parts=indent['questions'],
+            message_texts=(indent['answer'],)
+        )
